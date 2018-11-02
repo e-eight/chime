57bcbfe41f8aabcea9ad14a01d0e28bf4ea37213
soham <soham@posteo.net>
2018-11-02 17:35:50 -0500

    Removed copies of src files

--- operator_r_sq.h
+++
@@ -1,8 +0,0 @@
-#ifndef __OPERATOR_R_SQ_H__
-#define __OPERATOR_R_SQ_H__
-
-typedef struct integrand_l_params integrand_l_params;
-struct integrand_l_params { int n; double alpha; int np; double alphap; };
-double integral_l1(wf_params *i_params, wf_params *f_params);
-
-#endif
