8ad842004270bf7e073f4cb91498455e2af451a0
soham <soham@posteo.net>
2018-12-21 03:23:31 -0600

    removed old_c

--- old_c/constants.c
+++
@@ -1,8 +0,0 @@
-#include "constants.h"
-
-const double PION_MASS = 134.9770; // (in MeV/c^2)
-const double RED_NUCLEON_MASS = 469.4593340; // (in MeV/c^2)
-const double HBARC = 197.3269602; // (in MeV fm)
-
-/* The isoscalar charge radius for the nucleon im fm^2. */
-const double R_ES_SQUARED = 0.777; 
