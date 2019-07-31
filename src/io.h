35e6ea3e87a87086e4d06d9799541e946d7b7469
soham <soham@posteo.net>
2019-07-31 16:09:26 -0500

    Removed unnecessary variant classes from `io.h`.

--- src/io.h
+++ src/io.h
@@ -4,74 +4,14 @@
 #include <chrono>
 #include <iomanip>
 #include <array>
-#include <boost/variant.hpp>
 #include "basis/lsjt_scheme.h"
 #include "basis/lsjt_operator.h"
 #include "fmt/format.h"
 #include "chiral.h"
-#include "constants.h"
 #include "utility.h"
 
 namespace io
 {
-  // Variant classes for holding both relative and relative-cm variants.
-  // using LSJTSpace = boost::variant<basis::RelativeSpaceLSJT,
-  //                                  basis::RelativeCMSpaceLSJT>;
-  // using LSJTParameters = boost::variant<basis::RelativeOperatorParametersLSJT,
-  //                                       basis::RelativeCMOperatorParametersLSJT>;
-  // using LSJTSectors = boost::variant<std::array<basis::RelativeSectorsLSJT, 3>,
-  //                                    std::array<basis::RelativeCMSectorsLSJT, 3>>;
-  // using OscillatorLength = boost::variant<double, std::array<double, 2>>;
-
-  // // Choose the LSJTParameter type depending on whether cm is there or not.
-  // inline void SetLSJTParameters(LSJTParameters& params,
-  //                        basis::OperatorLabelsJT& op_labels,
-  //                        int& Nmax, int& Jmax, bool& has_cm)
-  // {
-  //   if (has_cm)
-  //       params = basis::RelativeCMOperatorParametersLSJT(op_labels, Nmax);
-  //   else
-  //       params = basis::RelativeOperatorParametersLSJT(op_labels, Nmax, Jmax);
-  // }
-
-  // // Choose the LSJTSpace type depending on whether cm is there or not.
-  // inline void SetLSJTSpace(LSJTSpace& space, int& Nmax, int& Jmax, bool& has_cm)
-  // {
-  //   if (has_cm)
-  //     space = basis::RelativeCMSpaceLSJT(Nmax);
-  //   else
-  //     space = basis::RelativeSpaceLSJT(Nmax, Jmax);
-  // }
-
-  // inline void SetLSJTSectors(LSJTSectors& sectors, bool& has_cm)
-  // {
-  //   if (has_cm)
-  //     {
-  //       sectors = util::GetArray<basis::RelativeCMSectorsLSJT, 3>();
-  //     }
-  //   else
-  //     {
-  //       sectors = util::GetArray<basis::RelativeSectorsLSJT, 3>();
-  //     }
-  // }
-
-  // inline void SetOscillatorLength(OscillatorLength& b, double& hw, bool& has_cm)
-  // {
-  //   double b_rel = constants::hbarc
-  //     / std::sqrt(constants::reduced_nucleon_mass_MeV * hw);
-  //   if (!has_cm)
-  //     {
-  //       b = b_rel;
-  //     }
-  //   else
-  //     {
-  //       double b_cm = constants::hbarc
-  //         / std::sqrt(2 * constants::nucleon_mass_MeV * hw);
-  //       std::array<double, 2> b2 = {b_rel, b_cm};
-  //       b = b2;
-  //     }
-  // }
-
   void PrintTruncationInfo(basis::RelativeOperatorParametersLSJT& params)
   {
       fmt::print("Truncation: Nmax {0} Jmax {1} T0_max {2}\n",
